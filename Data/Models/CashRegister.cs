9f345868a167fadcb29b4e2f5303c1f03940b6cd
Pascal Schiwon <pascal.schiwon@schleuniger.com>
2024-10-09 21:58:17 +0200

    implement first changes to switch pages and design to mudblazor

---
+++ Data/Models/CashRegister.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TTCCashRegister.Data.Models
+{
+    public class CashRegister
+    {
+        [Key]
+        public int ID { get; set; }
+        [Column(TypeName = "decimal(5,2)")]
+        public decimal CurrentBalance { get; set; } = decimal.Zero;
+        public ICollection<Entry> Entries { get; set; }
+
+        public CashRegister()
+        {
+            Entries = new List<Entry>();
+
+        }
+    }
+}
