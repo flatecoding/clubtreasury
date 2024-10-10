fcdef2fd8c93b7569720b97a50e8fa4a7a899ab2
Pascal Schiwon <pascal.schiwon@schleuniger.com>
2024-10-10 09:36:06 +0200

    Replace pw in plain text with .net user-secrets

--- Data/CashDataContext.cs
+++ Data/CashDataContext.cs
@@ -1,24 +1,18 @@
-﻿using System.Data;
-//using System.Data.Entity;
-using Microsoft.EntityFrameworkCore;
-//using Microsoft.Extensions.Hosting;
+﻿using Microsoft.EntityFrameworkCore;
 using TTCCashRegister.Data.Models;
 
 namespace TTCCashRegister.Data
 {
     public class CashDataContext: DbContext
     {
-        static readonly string connectionString = "Server=192.168.10.51; User ID=dev; Password=Malcom_1%9; Database=TTCCash";
-
         public DbSet<Entry> Entries { get; set; }
         public DbSet<BusinessSector> BusinessSectors { get; set; }
+        public DbSet<CashRegister> CashRegister { get; set; } = default!;
 
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public CashDataContext(DbContextOptions<CashDataContext> options)
+           : base(options)
         {
-            optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
         }
-        public DbSet<TTCCashRegister.Data.Models.CashRegister> CashRegister { get; set; } = default!;
 
     }
 
