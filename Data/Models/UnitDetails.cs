8c0c98fcf12f9f6f39c0c85cf1179e323523b7d5
Pascal Schiwon <pascal.schiwon@schleuniger.com>
2024-10-11 16:36:25 +0200

    Some improvements and name changes

---
+++ Data/Models/UnitDetails.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TTCCashRegister.Data.Models
+{
+    public class UnitDetails
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string CostDetails { get; set; } = string.Empty;
+        public int CostUnitId { get; set; }
+        [ForeignKey("CostUnitId")]
+        public CostUnit? CostUnit { get; set; }
+    }
+}
