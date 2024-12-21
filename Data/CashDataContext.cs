f388ee4d37300f4e5ee92a8868c241e57ce71baa
Schiwi <schiwon.pascal@gmx.de>
2024-12-21 15:00:16 +0100

    implement first prts fro fluent validation and move files in seperate folder

--- Data/CashDataContext.cs
+++ Data/CashDataContext.cs
@@ -5,7 +5,7 @@ namespace TTCCashRegister.Data
 {
     public class CashDataContext : DbContext
     {
-        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Transaction.TransactionModel> Transactions { get; set; }
         public DbSet<CostUnit> CostUnits { get; set; }
         public DbSet<CashRegister> CashRegisters { get; set; } = default!;
         public DbSet<UnitDetails> UnitDetails { get; set; }
