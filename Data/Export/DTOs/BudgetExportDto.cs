9610145e3403ec2dcaffe192ddf2c74931becded
Siphie <schiwon.pascal@web.de>
2025-11-28 17:40:26 +0100

    Add Dtos and ExportToExcel

---
+++ Data/Export/DTOs/BudgetExportDto.cs
@@ -0,0 +1,43 @@
+namespace TTCCashRegister.Data.Export.DTOs;
+
+public class BudgetFlatEntryDto
+{
+    public int CostCenterId { get; set; }
+    public string CostCenterName { get; set; } = string.Empty;
+
+    public int CategoryId { get; set; }
+    public string CategoryName { get; set; } = string.Empty;
+
+    public int ItemDetailId { get; set; }
+    public string ItemDetailName { get; set; } = string.Empty;
+
+    public decimal Amount { get; set; }
+
+    public int? PersonId { get; set; }
+    public string? PersonName { get; set; }
+}
+
+
+    public class CostCenterDto
+    {
+        public int Id { get; set; }
+        public string CostUnitName { get; set; } = "";
+    }
+
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+    }
+
+    public class ItemDetailDto
+    {
+        public int Id { get; set; }
+        public string CostDetails { get; set; } = "";
+    }
+
+    public class PersonDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+    }
