3f34f3fb884e5d72770946be4368991a1c6a3981
Siphie <schiwon.pascal@web.de>
2026-02-02 08:14:03 +0100

    Rename filter functions to more meaningful names

--- Data/Transaction/TransactionService.cs
+++ Data/Transaction/TransactionService.cs
@@ -1,5 +1,4 @@
-﻿using System.Diagnostics;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
 using MudBlazor;
 using TTCCashRegister.Data.Allocation;
