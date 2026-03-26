348c834e742e52b97ad82c978ef3422e6d2df855
flatecoding <schiwon.pascal@web.de>
2026-03-26 13:43:46 +0100

    Compute cash register balances via SQL instead of loading all transactions

--- Data/CashRegister/ICashRegisterService.cs
+++ Data/CashRegister/ICashRegisterService.cs
@@ -5,6 +5,7 @@ namespace ClubTreasury.Data.CashRegister;
 public interface ICashRegisterService
 {
     Task<List<CashRegisterModel>> GetAllCashRegistersAsync(CancellationToken ct = default);
+    Task<Dictionary<int, decimal>> GetCashRegisterBalancesAsync(CancellationToken ct = default);
     Task<CashRegisterModel?> GetCashRegisterByIdAsync(int id, CancellationToken ct = default);
     Task<CashRegisterModel?> GetFirstCashRegisterAsync(CancellationToken ct = default);
     Task<Result> AddCashRegisterAsync(CashRegisterModel cashRegisterModel, CancellationToken ct = default);
